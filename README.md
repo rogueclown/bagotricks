fd740ac9f4d575fb0c89a4b6a25d025c1677e135
nicolle neulist <rogueclown@rogueclown.net>
2013-03-28 17:54:16 -0600

    first script

---
+++ README.md
@@ -0,0 +1,4 @@
+as the title implies, this repository is a mixed bag of python scripts,
+generally focused at automating common repetitive pen testing tasks.
+
+* headerchecker.py: requires curl.
---
+++ headerchecker.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+# headerchecker.py
+#
+# checks http headers for common flaws such as lack of framebusting controls, 
+# lack of secure and httponly cookie flags, and lack of HSTS implementation.
+#
+# rogueclown, march 2013
+# licensed under the WTFPL, http://www.wtfpl.net/txt/copying.
+
+import subprocess
+import re
+from optparse import OptionParser
+from sys import exit
+
+parser = OptionParser(usage='usage: %prog -i url_list')
+parser.add_option('-i', '--inputfile', dest='infile', help='list of URLs.  one per line.  needs http or https prefix.')
+
+(opts, args) = parser.parse_args()
+
+if not opts.infile:
+	print 'input file option (-i) is mandatory.  use -h switch for help.'
+	sys.exit(1)
+
+try:
+	f = open(opts.infile)
+	urls = f.readlines()
+	f.close()
+except:
+	print 'error reading ' + opts.infile
+	sys.exit(1)
+
+urls = [url.rstrip() for url in urls]
+
+def extractcookiename(header):
+	r =re.compile('\s(.*?)=')
+	m = r.search(header)
+	cookiename = m.group(0)[1:-1]
+	return cookiename
+
+for url in urls:
+	print "=" * 60 + '\n'
+	print url + '\n'
+	rawheaders = subprocess.check_output(['curl', '--insecure', '-s', '-I', url])
+	headers = rawheaders.split('\n')
+	if "x-frame-options" not in rawheaders.lower():
+		print 'x-frame-options header not present; site may be vulnerable to clickjacking.'
+	if "https" in url.lower():
+		if 'strict-transport-security' not in rawheaders.lower():
+			print 'HSTS header not present.'
+	for header in headers:
+		if 'set-cookie' in header.lower():
+			if 'httponly' not in header.lower():
+				print 'HttpOnly option missing from ' + extractcookiename(header) + 'header.'
+			if 'https' in url.lower():
+				if 'secure' not in header.lower():
+					print 'Secure option missing from ' + extractcookiename(header) + 'header.'
+
+print '=' * 60 + '\n'
