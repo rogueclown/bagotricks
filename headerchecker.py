a22eb2a80b6656aaffe835649c371d89a0d3fa89
nicolle neulist <rogueclown@rogueclown.net>
2013-03-28 21:13:54 -0600

    added error checking for header retrieval

--- headerchecker.py
+++ headerchecker.py
@@ -41,19 +41,21 @@ def extractcookiename(header):
 for url in urls:
 	print "=" * 60 + '\n'
 	print url + '\n'
-	rawheaders = subprocess.check_output(['curl', '--insecure', '-s', '-I', url])
-	headers = rawheaders.split('\n')
-	if "x-frame-options" not in rawheaders.lower():
-		print 'x-frame-options header not present; site may be vulnerable to clickjacking.'
-	if "https" in url.lower():
-		if 'strict-transport-security' not in rawheaders.lower():
-			print 'HSTS header not present.'
-	for header in headers:
-		if 'set-cookie' in header.lower():
-			if 'httponly' not in header.lower():
-				print 'HttpOnly option missing from ' + extractcookiename(header) + 'header.'
-			if 'https' in url.lower():
-				if 'secure' not in header.lower():
-					print 'Secure option missing from ' + extractcookiename(header) + 'header.'
-
+	try:
+		rawheaders = subprocess.check_output(['curl', '--insecure', '-s', '-I', url])
+		headers = rawheaders.split('\n')
+		if "x-frame-options" not in rawheaders.lower():
+			print 'x-frame-options header not present; site may be vulnerable to clickjacking.'
+		if "https" in url.lower():
+			if 'strict-transport-security' not in rawheaders.lower():
+				print 'HSTS header not present.'
+		for header in headers:
+			if 'set-cookie' in header.lower():
+				if 'httponly' not in header.lower():
+					print 'HttpOnly option missing from ' + extractcookiename(header) + 'header.'
+				if 'https' in url.lower():
+					if 'secure' not in header.lower():
+						print 'Secure option missing from ' + extractcookiename(header) + 'header.'
+	except:
+		print "http headers could not be retrieved for " + url
 print '=' * 60 + '\n'
