30cde3c09914ea9681ec3f2198293bc381b94d21
nicolle neulist <rogueclown@rogueclown.net>
2013-04-04 18:41:03 -0600

    added ability to sort output by vuln in headerchecker.py

--- headerchecker.py
+++ headerchecker.py
@@ -15,6 +15,7 @@ from sys import exit
 
 parser = OptionParser(usage='usage: %prog -i url_list')
 parser.add_option('-i', '--inputfile', dest='infile', help='list of URLs.  one per line.  needs http or https prefix.')
+parser.add_option('-s', '--sorting', dest='sort', help='sort format.  choose "host" for sorting by host, or "vuln" for sorting by vulnerability.  defaults to sorting by vulnerability.')
 
 (opts, args) = parser.parse_args()
 
@@ -30,6 +31,18 @@ except:
 	print 'error reading ' + opts.infile
 	exit(1)
 
+if opts.sort == 'host':
+	sort = 'host'
+else:
+	sort = 'vuln'
+
+if sort == 'vuln':
+	clickjack = []
+	hstsabsent = []
+	nosecure = []
+	nohttponly = []
+	noheaders = []
+
 urls = [url.rstrip() for url in urls]
 
 def extractcookiename(header):
@@ -39,23 +52,60 @@ def extractcookiename(header):
 	return cookiename
 
 for url in urls:
-	print "=" * 60 + '\n'
-	print url + '\n'
+	if sort != 'vuln':
+		print "=" * 60 + '\n'
+		print url + '\n'
 	try:
 		rawheaders = subprocess.check_output(['curl', '--insecure', '--max-time', '5', '--connect-timeout', '0', '-s', '-I', url])
 		headers = rawheaders.split('\n')
 		if "x-frame-options" not in rawheaders.lower():
-			print 'x-frame-options header not present; site may be vulnerable to clickjacking.'
+			if sort == 'vuln':
+				clickjack.append(url)
+			else:
+				print 'x-frame-options header not present; site may be vulnerable to clickjacking.'
 		if "https" in url.lower():
 			if 'strict-transport-security' not in rawheaders.lower():
-				print 'HSTS header not present.'
+				if sort == 'vuln':
+					hstsabsent.append(url)
+				else:
+					print 'HSTS header not present.'
 		for header in headers:
 			if 'set-cookie' in header.lower():
 				if 'httponly' not in header.lower():
-					print 'HttpOnly option missing from ' + extractcookiename(header) + 'header.'
+					if sort == 'vuln':
+						nohttponly.append(url + ' (cookie: ' + extractcookiename(header) + ')')
+					else:
+						print 'HttpOnly option missing from ' + extractcookiename(header) + ' cookie.'
 				if 'https' in url.lower():
 					if 'secure' not in header.lower():
-						print 'Secure option missing from ' + extractcookiename(header) + 'header.'
+						if sort == 'vuln':
+							nosecure.append(url + ' (cookie: ' + extractcookiename(header) + ')')
+						else:
+							print 'Secure option missing from ' + extractcookiename(header) + ' cookie.'
 	except:
-		print "http headers could not be retrieved for " + url
+		if sort == 'vuln':
+			noheaders.append(url)
+		else:		
+			print "http headers could not be retrieved for " + url
 print '=' * 60 + '\n'
+
+# print output if sorted by vuln
+if sort == 'vuln':
+	print 'x-frame-options header not present; site may be vulnerable to clickjacking:\n'
+	for url in clickjack:
+		print url
+	print '=' * 60
+	print '\n'
+	print 'HSTS neader not present:\n'
+	for url in hstsabsent:
+		print url
+	print '=' * 60
+	print '\n'
+	print 'HttpOnly option missing:\n'
+	for url in nohttponly:
+		print url
+	print '=' * 60
+	print '\n'
+	print 'Secure option missing:\n'
+	for url in nosecure:
+		print url
